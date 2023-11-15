0e292014d75381f43ac651402ca4d10452a3d4e0
إبراهيم حسن محمد حسن <ibrahimhassannamr@gmail.com>
2023-11-15 21:21:33 +0200

    is valid big real function

--- BigReal.cpp
+++ BigReal.cpp
@@ -1,5 +1,24 @@
-//
-// Created by Hema on 11/15/2023.
-//
-
 #include "BigReal.h"
+
+//___________________________________________isValidReal
+bool BigReal :: isValidReal (string realNumber){
+    int DotsNum{0};
+    for(unsigned long long i{1}; i < realNumber.size(); i++){
+        // loop on the string if u found anything other the dot and the numbers
+        // or more than one dot return false
+        if(((int)realNumber[i] >= 48 && (int)realNumber[i] <= 57) || realNumber[i] == '.'){
+            if(realNumber[i] == '.') {
+                dotIndex = i;
+                DotsNum++;
+                if(DotsNum > 1) return false;
+            }
+        }
+        else return false;
+        //_______________________________________________________
+    }
+
+    // if the user didn't add a sign and it's a valid number
+    if((int)realNumber[0] >= 48 && (int)realNumber[0] <= 57 || realNumber[0] == '.') return true;
+    if(realNumber[0] == '+' || realNumber[0] == '-') return true; // if the first index is sign
+    return false;
+}
